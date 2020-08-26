782600b99bed7b28fb5857d7fcc21e32357e3de6
xtrtxllx <shazwanimizainiK15@gmail.com>
2020-08-26 15:46:07 +0800

    Init project

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative ”boot”
+
+require ”rails/all”
+
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+   
+    config.load_defaults 5.2
+
+  end
+end
