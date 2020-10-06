504f4b6f63ed4a76afd6e2a8bcb38a266af87a8b
xtrxllx <shazwanimizainiK15@gmail.com>
2020-10-06 15:19:45 +0800

    chapter9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      params[:session][:remember_me] == Settings.remember_me ? remember(@user) : forget(@user)
       redirect_to user_path(id: current_user.id)
     else
       flash.now[:danger] = t ("login.fail")
