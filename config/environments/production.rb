08a5b64b8d9474b33a651b02800ee6eb6385c7c1
xtrxllx <shazwanimizainiK15@gmail.com>
2020-10-07 17:21:06 +0800

    chapter11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,9 +58,21 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sample_app_production"
+  # config.active_job.queue_name_prefix = "sampleApp_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "<your heroku app>.herokuapp.com"
+  config.action_mailer.default_url_options = {host: host}
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV["USER_EMAIL"],
+    password: ENV["USER_PASSWORD"],
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/home", to: "chapter3#home"
   resources :users
+  resources :account_activations, only: :edit
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
