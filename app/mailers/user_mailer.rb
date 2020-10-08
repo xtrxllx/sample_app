7f8a6df8c16969942bdbfa13dd2b8117aaca16f8
xtrxllx <shazwanimizainiK15@gmail.com>
2020-10-08 11:48:50 +0800

    chapter12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
-  def account_activation user
-    @user = user
-    mail to: user.email, subject: "Account activation"
+    def account_activation user
+      @user = user
+      mail to: user.email, subject: "Account activation"
+    end
+  
+    def password_reset user
+      @user = user
+      mail to: user.email, subject: t("forget.reset.title")
+    end
   end
-end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/home", to: "chapter3#home"
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets, except: %i(show index destroy)
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
